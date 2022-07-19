a9e9de8f95f6daa4b9837d8d09d7c934f376a031
Conor Flynn <conor@glimpse.ie>
2022-07-19 21:37:46 +0100

    Uses Settings fragment with SharedPreferences.

--- app/src/main/java/com/example/roadquality/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/roadquality/ui/home/HomeFragment.java
@@ -1,17 +1,12 @@
 package com.example.roadquality.ui.home;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,117 +17,18 @@ import com.example.roadquality.services.MainService;
 import com.google.android.material.slider.LabelFormatter;
 import com.google.android.material.slider.Slider;
 
-import java.util.logging.Logger;
-
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.EasyPermissions;
-
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-    private boolean running = false;
-
-    // Necessary for EasyPermissions to work right.
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // Forward results to EasyPermissions
-
-        Log.println(Log.ASSERT, "s", "Permissions:");
-        for (int i = 0; i < permissions.length; i++) {
-            Log.println(Log.ASSERT, "s", permissions[i]);
-        }
-
-        Log.println(Log.ASSERT, "s", "grant results:");
-        for (int i = 0; i < grantResults.length; i++) {
-            Log.println(Log.ASSERT, "s", String.valueOf(grantResults[i]));
-        }
-
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
-    }
-
-    @AfterPermissionGranted(109)
-    private void verifyStorageLocationPermissions() {
-        String[] perms = {
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        };
-
-        if (!EasyPermissions.hasPermissions(this.getActivity(), perms)) {
-            EasyPermissions.requestPermissions(
-                    this,
-                    "Please grant the following permission for via to function",
-                    109,
-                    perms
-            );
-        } else {
-            Log.println(Log.ASSERT, "s", "HAVE PERMS");
-        }
-    }
-
 
     public View onCreateView(
             @NonNull LayoutInflater inflater,
             ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        // Set a LabelFormatter on the Sliders to not show ugly floats.
-        Slider minutesToCutSlider = binding.minutesToCutSlider;
-        Slider metresToCutSlider = binding.metresToCutSlider;
-
-        LabelFormatter sliderInputLabelFormatter = new LabelFormatter() {
-            @NonNull
-            @Override
-            public String getFormattedValue(float value) {
-                return String.valueOf(Math.round(value));
-            }
-        };
-        minutesToCutSlider.setLabelFormatter(sliderInputLabelFormatter);
-        metresToCutSlider.setLabelFormatter(sliderInputLabelFormatter);
-
-        // "Enhanced Privacy" is simply sending partials and relative times.
-        CheckBox enhancedPrivacyCheckbox = binding.enhancedPrivacyCheckBox;
-
-        Button startStop = root.findViewById(R.id.start_stop_button);
-
-        final Button button = binding.startStopButton;
-        button.setOnClickListener(v -> {
-            if (!running) {
-                verifyStorageLocationPermissions();
-
-                startStop.setText("Stop Journey");
-
-                String transportType = "bike";
-                boolean sendRelativeTime = enhancedPrivacyCheckbox.isChecked();
-                boolean sendPartials = enhancedPrivacyCheckbox.isChecked();
-                int minutesToCut = Math.round(minutesToCutSlider.getValue());
-                int metresToCut = Math.round(metresToCutSlider.getValue());
-
-                Intent mainService = new Intent(getActivity(), MainService.class);
-                mainService.putExtra("transportType", transportType);
-                mainService.putExtra("suspension", Boolean.FALSE);
-                mainService.putExtra("sendRelativeTime", sendRelativeTime);
-                mainService.putExtra("minutesToCut", minutesToCut);
-                mainService.putExtra("metresToCut", metresToCut);
-                mainService.putExtra("sendPartials", sendPartials);
-
-                // DELETE ON SEND? - not really important
-
-                getActivity().startForegroundService(mainService);
-                running = true;
-            } else {
-                getActivity().stopService(new Intent(getActivity(), MainService.class));
-                running = false;
-                startStop.setText("Start Journey");
-            }
-        });
 
         return root;
     }
--- app/src/main/java/com/example/roadquality/ui/home/SettingsFragment.java
+++ app/src/main/java/com/example/roadquality/ui/home/SettingsFragment.java
@@ -1,66 +1,116 @@
 package com.example.roadquality.ui.home;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.example.roadquality.R;
+import com.example.roadquality.databinding.FragmentHomeBinding;
+import com.example.roadquality.databinding.FragmentSettingsBinding;
+import com.example.roadquality.services.MainService;
+import com.google.android.material.slider.LabelFormatter;
+import com.google.android.material.slider.Slider;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingsFragment extends Fragment {
+    private FragmentSettingsBinding binding;
+    private boolean running = false;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        // Get the SharedPreferences:
+        SharedPreferences preferences = getActivity().getSharedPreferences("Via Preferences", MODE_PRIVATE);
+        // This is just a default that should always be set. If not, set all the things we need:
+        if (!preferences.contains("metresToCut")) {
+            preferences.edit()
+                    .putInt("metresToCut", 200)
+                    .putInt("minutesToCut", 2)
+                    .putBoolean("enhancedPrivacy", false)
+                    .apply();
         }
+
+        // Bind the input components:
+        Slider minutesToCutSlider = binding.minutesToCutSlider;
+        Slider metresToCutSlider = binding.metresToCutSlider;
+        // "Enhanced Privacy" is simply sending partials and relative times.
+        CheckBox enhancedPrivacyCheckbox = binding.enhancedPrivacyCheckBox;
+
+        // Initialize inputs with values from SharedPreferences:
+        metresToCutSlider.setValue((float) preferences.getInt("metresToCut", 200));
+        minutesToCutSlider.setValue((float) preferences.getInt("minutesToCut", 2));
+        enhancedPrivacyCheckbox.setChecked(preferences.getBoolean("enhancedPrivacy", false));
+
+        // And bind changes to the inputs to the SharedPreferences:
+        metresToCutSlider.addOnChangeListener((slider, value, fromUser) -> preferences.edit().putInt("metresToCut", Math.round(value)).apply());
+        minutesToCutSlider.addOnChangeListener((slider, value, fromUser) -> preferences.edit().putInt("minutesToCut", Math.round(value)).apply());
+        enhancedPrivacyCheckbox.setOnCheckedChangeListener((compoundButton, b) -> preferences.edit().putBoolean("enhancedPrivacy", b).apply());
+
+        // Set a LabelFormatter on the Sliders to not show ugly floats.
+        LabelFormatter sliderInputLabelFormatter = value -> String.valueOf(Math.round(value));
+        minutesToCutSlider.setLabelFormatter(sliderInputLabelFormatter);
+        metresToCutSlider.setLabelFormatter(sliderInputLabelFormatter);
+
+
+
+        // TODO: not yet refactored
+        Button startStop = root.findViewById(R.id.start_stop_button);
+
+        final Button button = binding.startStopButton;
+        button.setOnClickListener(v -> {
+            /*
+            if (!running) {
+                startStop.setText("Stop Journey");
+
+                String transportType = "bike";
+                boolean sendRelativeTime = enhancedPrivacyCheckbox.isChecked();
+                boolean sendPartials = enhancedPrivacyCheckbox.isChecked();
+                int minutesToCut = Math.round(minutesToCutSlider.getValue());
+                int metresToCut = Math.round(metresToCutSlider.getValue());
+
+                Intent mainService = new Intent(getActivity(), MainService.class);
+                mainService.putExtra("transportType", transportType);
+                mainService.putExtra("suspension", Boolean.FALSE);
+                mainService.putExtra("sendRelativeTime", sendRelativeTime);
+                mainService.putExtra("minutesToCut", minutesToCut);
+                mainService.putExtra("metresToCut", metresToCut);
+                mainService.putExtra("sendPartials", sendPartials);
+
+                // DELETE ON SEND? - not really important
+
+                getActivity().startForegroundService(mainService);
+                running = true;
+            } else {
+                getActivity().stopService(new Intent(getActivity(), MainService.class));
+                running = false;
+                startStop.setText("Start Journey");
+            }
+             */
+        });
+
+        return root;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
